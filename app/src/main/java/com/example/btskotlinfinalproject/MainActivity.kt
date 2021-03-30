99cc370ef056e4b001523ec49871ae8ed49802c0
ChangingColor <fmensa2@uic.edu>
2021-03-29 19:10:40 -0500

    MainActivity_update

--- app/src/main/java/com/example/btskotlinfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/btskotlinfinalproject/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.btskotlinfinalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    // these variables are going to be used to store the club's information
+    // val programName = "HI"
+    // val programDescription = "This is a great club!"
+    // val programContact = "fmesa2@uic.edu"
+    // val programRequirements = "You must be awesome"
+
+    private var TAG:String = "MainActivity"
+
+    private lateinit var button: Button
+    private lateinit var backButton: Button
+    private lateinit var contactButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.start_bttn)
+        button.setOnClickListener {
+            Log.d(TAG,"CHANGE NOW")
+        }
+
+        backButton = findViewById(R.id.back_button)
+        backButton.setOnClickListener {
+            Log.d(TAG,"You have pressed the back button, here is the log!!")
+        }
+
+        contactButton = findViewById(R.id.contact_button)
+        contactButton.setOnClickListener {
+            Log.d(TAG,"EMAILING")
+        }
     }
+
 }
