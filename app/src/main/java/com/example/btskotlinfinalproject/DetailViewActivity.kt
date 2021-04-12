a01242eee73e5061ca5783ece4cc6968daca668d
Lucas Oskorep <lucas.oskorep@gmail.com>
2021-04-12 18:56:37 -0500

    Email App updates

--- app/src/main/java/com/example/btskotlinfinalproject/DetailViewActivity.kt
+++ app/src/main/java/com/example/btskotlinfinalproject/DetailViewActivity.kt
@@ -1,9 +1,9 @@
 package com.example.btskotlinfinalproject
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -20,15 +20,15 @@ class DetailViewActivity : AppCompatActivity() {
         ;
 
         val extras = intent.extras
-        var mName = extras?.getString("name")
-
+        val mName = extras?.getString("name")
         val mDesc = extras?.getString("desc")
+        val mEmail = extras?.getString("email")
 
         val image = findViewById<ImageView>(R.id.logoView)
         val resourceImage: Int = this.resources.getIdentifier(extras?.getString("logo"), "drawable", this.packageName)
         image.setImageResource(resourceImage)
 
-        val textView = findViewById<TextView>(R.id.ProgramName).apply { text = mName }
+        findViewById<TextView>(R.id.ProgramName).apply { text = mName }
 
         val textViewDesc = findViewById<TextView>(R.id.ProgramDescription).apply { text = mDesc }
         textViewDesc.movementMethod = ScrollingMovementMethod()
@@ -36,11 +36,19 @@ class DetailViewActivity : AppCompatActivity() {
 
         val contactButton = findViewById<Button>(R.id.contact_button)
         contactButton.setOnClickListener {
-          Log.d(TAG, "Opening email intent")
-            val email = extras?.getString("email")
-            val intent = Intent(this, EmailActivity::class.java)
-            intent.putExtra("to", email)
-            startActivity(intent)
+            if (mEmail != null) {
+                val selectorIntent = Intent(Intent.ACTION_SENDTO)
+                selectorIntent.data = Uri.parse("mailto:")
+
+                val emailIntent = Intent(Intent.ACTION_SEND)
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(mEmail))
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Request to Join $mName")
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Hey $mName,\n\n" +
+                        "I was emailing to find out more about your organization and inquire about joining.\n\n" +
+                        "Thanks,")
+                emailIntent.selector = selectorIntent
+                startActivity(Intent.createChooser(emailIntent, "Send email..."))
+            }
         }
     }
 }
